aaab9ec71f7843e2032cdc3316b3c2857e04c773
Flávio Carvalho <flaviocfr@gmail.com>
2019-09-19 12:47:30 -0300

    Post

--- src/main/java/com/reobotnet/comercialapi/controller/OportunidadeController.java
+++ src/main/java/com/reobotnet/comercialapi/controller/OportunidadeController.java
@@ -3,11 +3,17 @@ package com.reobotnet.comercialapi.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.reobotnet.comercialapi.model.Oportunidade;
@@ -33,6 +39,12 @@ public class OportunidadeController {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(oportunidade.get());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Oportunidade adicionar(@Valid @RequestBody Oportunidade oportunidade) {
+		return oportunidadeRepository.save(oportunidade);
 	}
 
 }
