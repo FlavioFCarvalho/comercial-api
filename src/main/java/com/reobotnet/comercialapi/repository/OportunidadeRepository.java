260aeea4ece2fdd43c83e54a620a145afb45d15a
Flávio Carvalho <flaviocfr@gmail.com>
2019-09-19 14:53:20 -0300

    Consulta por Descricao e nomeProspecto

--- src/main/java/com/reobotnet/comercialapi/repository/OportunidadeRepository.java
+++ src/main/java/com/reobotnet/comercialapi/repository/OportunidadeRepository.java
@@ -1,9 +1,11 @@
 package com.reobotnet.comercialapi.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.reobotnet.comercialapi.model.Oportunidade;
 
 public interface OportunidadeRepository extends JpaRepository<Oportunidade, Long>{
-
-}
+ Optional<Oportunidade> findByDescricaoAndNomeProspecto(String descricao, String nomeProspecto);
+ }
