e0983371f7815949a0c8c920b101e9ff78a022bf
Flávio Carvalho <flaviocfr@gmail.com>
2019-09-19 12:01:45 -0300

    Retorno da lista de oportunidades

--- src/main/java/com/reobotnet/comercialapi/controller/OportunidadeController.java
+++ src/main/java/com/reobotnet/comercialapi/controller/OportunidadeController.java
@@ -1,16 +1,25 @@
 package com.reobotnet.comercialapi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.reobotnet.comercialapi.model.Oportunidade;
+import com.reobotnet.comercialapi.repository.OportunidadeRepository;
 
 @RestController
 @RequestMapping("/oportunidades")
 public class OportunidadeController {
 	
+	@Autowired
+	private OportunidadeRepository oportunidadeRepository;
+	
 	@GetMapping
-	public String Listar() {
-		return "Eu nasci!!!";
+	public List<Oportunidade> Listar() {
+		return oportunidadeRepository.findAll();
 	}
 
 }
