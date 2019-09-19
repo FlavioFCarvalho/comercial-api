b620af16f06cfadf8bd2a5e62459f0131534d4de
Flávio Carvalho <flaviocfr@gmail.com>
2019-09-19 11:34:35 -0300

    Mapeando a tabela oportunidade.

--- src/main/java/com/reobotnet/comercialapi/model/Oportunidade.java
+++ src/main/java/com/reobotnet/comercialapi/model/Oportunidade.java
@@ -2,8 +2,14 @@ package com.reobotnet.comercialapi.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 public class Oportunidade {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nomeProspector;
 	private String descricao;
