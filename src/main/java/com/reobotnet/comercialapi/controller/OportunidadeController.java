5ee6be683c79ef983133d4ef3f09c663fac7da06
Flávio Carvalho <flaviocfr@gmail.com>
2019-09-19 12:21:13 -0300

    Busca por id

--- src/main/java/com/reobotnet/comercialapi/controller/OportunidadeController.java
+++ src/main/java/com/reobotnet/comercialapi/controller/OportunidadeController.java
@@ -1,9 +1,12 @@
 package com.reobotnet.comercialapi.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,16 @@ public class OportunidadeController {
 	@GetMapping
 	public List<Oportunidade> Listar() {
 		return oportunidadeRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity <Oportunidade> buscar(@PathVariable Long id){
+		Optional<Oportunidade> oportunidade =  oportunidadeRepository.findById(id);
+		
+		if(!oportunidade.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(oportunidade.get());
 	}
 
 }
