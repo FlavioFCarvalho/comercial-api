282d5936d084d858f96c96de2d34aa1f6d827f57
Flávio Carvalho <flaviocfr@gmail.com>
2019-09-19 11:39:52 -0300

    Criando a interface OportunidadeRepository

---
+++ src/main/java/com/reobotnet/comercialapi/repository/OportunidadeRepository.java
@@ -0,0 +1,9 @@
+package com.reobotnet.comercialapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.reobotnet.comercialapi.model.Oportunidade;
+
+public interface OportunidadeRepository extends JpaRepository<Oportunidade, Long>{
+
+}
