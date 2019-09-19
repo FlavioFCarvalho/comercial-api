38ef8e28d6a7afa199191df1fa9895ca88f1de55
Flávio Carvalho <flaviocfr@gmail.com>
2019-09-19 12:39:55 -0300

    Adicionando algumas validações no model Oportunidade.

--- src/main/java/com/reobotnet/comercialapi/model/Oportunidade.java
+++ src/main/java/com/reobotnet/comercialapi/model/Oportunidade.java
@@ -7,15 +7,24 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 @Entity
 public class Oportunidade {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotEmpty
+	@Size(max = 80)
 	@Column(name = "nome_prospecto")
 	private String nomeProspecto;
 	
+	@NotEmpty
+	@Size(max = 200)
 	private String descricao;
+
+	@Min(0)
 	private BigDecimal valor;
 	public Long getId() {
 		return id;
